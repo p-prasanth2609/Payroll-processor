8743fc57a15084cbf8ecd8b53ffb326fe28873bb
satheeshkumar <psatheesh21120@gamil.com>
2023-07-12 05:45:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+class SalariedEmployee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def CalculatePay(self):
+        return f"{self.name} got {self.salary} as a salary"
+
+
+class HourlyEmployee:
+    def __init__(self, name, HourlyRate, HoursWorked):
+        self.name = name
+        self.HourlyRate = HourlyRate
+        self.HoursWorked = HoursWorked
+
+    def CalculatePay(self):
+        return f"{self.name} got {self.HourlyRate * self.HoursWorked} as a salary"
+
+
+if __name__ == '__main__':
+    salary_Employee = SalariedEmployee("prasanth", 500000)
+    Hourly_Employee = HourlyEmployee("pp", 10000, 8)
+    print(salary_Employee.CalculatePay())
+    print(Hourly_Employee.CalculatePay())
